200fe7af9ecd4f9c54197fd573a6e9af66120978
ahmet <ahmet.ayyildiz@qlik.com>
2023-04-05 13:26:02 +0200

    toggle switch: create a dumb toggle component

--- src/containers/ThemeSwitcher.vue
+++ src/containers/ThemeSwitcher.vue
@@ -1,13 +1,23 @@
 <template>
   <div class="flex items-center space-x-4">
     <IconSun />
-    <DjSwitch />
+    <DjSwitch v-model="switchToggle" />
     <IconMoon />
   </div>
 </template>
 
 <script lang="ts" setup>
+// ref
+import { ref, watch } from "vue"
+
 import DjSwitch from '../components/DjSwitch.vue';
 import IconMoon from '../components/icons/IconMoon.vue';
 import IconSun from '../components/icons/IconSun.vue';
+
+
+const switchToggle = ref(false)
+
+watch(switchToggle, () => {
+  console.log('toggleable: ', switchToggle.value)
+})
 </script>
